c281eb3af7aa5f9d7f2f69e38a02b3ea7d7639cc
antoinefa <fabien.antoine@m4x.org>
2020-01-02 12:00:31 +0100

    align api path in js code to APP

--- src/runRequest.js
+++ src/runRequest.js
@@ -1,7 +1,7 @@
 export default async function runRequest(body) {
   const path = process.env.ES_PROXY_PATH;
 
-  const response = await fetch(`/search-ui/api/v0/search`, {
+  const response = await fetch(`${process.env.APP}/api/v0/search`, {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(body)
