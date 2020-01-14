341d828550d71266b4bcc63d2969c3cb3915a8db
cristianpb <cristianpb@users.noreply.github.com>
2020-01-14 16:50:52 +0000

    Automated deployment: Tue Jan 14 16:50:52 UTC 2020 0e15b42708a3646be6540bf12e8648f8ab90c2c8

---
+++ precache-manifest.ed8be7a2a87420cec03c4712ac433cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fb071f375143b926b7085900a1383d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "155bbaeb412372456613",
+    "url": "/static/css/main.88f60ff1.chunk.css"
+  },
+  {
+    "revision": "d938690e154293e5c941",
+    "url": "/static/js/2.c4cf51a7.chunk.js"
+  },
+  {
+    "revision": "2522ba5448754ad84fcfba0b29be1357",
+    "url": "/static/js/2.c4cf51a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "155bbaeb412372456613",
+    "url": "/static/js/main.604ecd86.chunk.js"
+  },
+  {
+    "revision": "aafac94dd385aa914919",
+    "url": "/static/js/runtime-main.3da4dd3f.js"
+  }
+]);
